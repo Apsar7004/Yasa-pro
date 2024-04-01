8becc04e31242f56375ffa5e899e872742dfae4f
Apsar7004 <115987441+Apsar7004@users.noreply.github.com>
2024-04-01 21:54:52 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 "use client"
-import Image from "next/image"
+
 import Section1 from "../components/home/Section1"
 import Section2 from "../components/home/Section2"
 import Section3 from "../components/home/Section3"
 import Section4 from "../components/home/Section4"
-import Link from "next/link"
+import Section5 from "../components/home/Section5"
+import Section6 from "../components/home/Section6"
+import Section7 from "../components/home/Section7"
+import Section8 from "../components/home/Section8"
 
 export default function Home() {
 
@@ -16,39 +19,13 @@ export default function Home() {
       <Section3 />
       <hr className="h-1"></hr>
       <Section4 />
-      <div>
-        <div className=" bg-yellow-500 py-16">
-        <h1 className=" text-2xl font-bold  text-center pb-10">LAUNCHING SOON</h1>
-        <div className="flex flex-col-reverse justify-around gap-10 md:flex-row">
-          <div className="my-auto mx-auto lg:mx-0">
-            <div>
-            <h1 className=" text-2xl font-semibold pb-6 max-md:text-center">LAUNCHING SOON</h1>
-            <p className="w-[300px] lg:w-[500px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic est et qui ullam iure, magnam sit ut quis voluptates, distinctio quam doloremque laborum repudiandae, earum quidem unde voluptatibus. Animi, optio.</p>
-            </div>
-            <div className="flex gap-3 pt-6 justify-around">
-              <Link href="/">
-            <Image src='/images/playstore.png'
-            alt="mobile image"
-            height={70}
-            width={150}/></Link>
-            <Link href='/about'>
-             <Image src='/images/appstore.png'
-            alt="mobile image"
-            height={70}
-            width={150}/></Link>
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Section8 />
 
-            </div>
-          </div>
-          <div className="mx-auto lg:mx-0">
-            <Image src='/images/phonemok.png'
-            alt="mobile image"
-            height={350}
-            width={350}/>
-          </div>
-        </div>
-        </div>
-      </div>
- 
+
+
   </>
   )
 }
